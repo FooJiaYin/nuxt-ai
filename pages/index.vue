cc1563523855d6bd82c42593138415341f9152d3
FooJiaYin <candy23198@hotmail.com>
2023-06-19 08:23:02 +0800

    feat(UI): Add prompt hints

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,8 @@
 <template>
+    <p>Try:</p>
+    <p><i>"Say hi"</i></p>
+    <p><i>"Greet the user with his favorite color"</i></p>
+    <p><i>"Display a joke with the color of sky"</i></p>
     <v-textarea label="Insert prompt here" v-model="input" variant="outlined" clearable></v-textarea>
     <v-btn @click="getResponse">Get Response</v-btn>
     <p>{{ response }}</p>
