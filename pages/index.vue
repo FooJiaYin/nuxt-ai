f93007e5d3f45bc13c2b14dbc19f33141ab55ba4
FooJiaYin <candy23198@hotmail.com>
2023-06-19 08:23:58 +0800

    feat(UI): Adjust layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
-    <p>Try:</p>
-    <p><i>"Say hi"</i></p>
-    <p><i>"Greet the user with his favorite color"</i></p>
-    <p><i>"Display a joke with the color of sky"</i></p>
-    <v-textarea label="Insert prompt here" v-model="input" variant="outlined" clearable></v-textarea>
-    <v-btn @click="getResponse">Get Response</v-btn>
-    <p>{{ response }}</p>
+    <v-container>
+        <p>Try:</p>
+        <p><i>"Say hi"</i></p>
+        <p><i>"Greet the user with his favorite color"</i></p>
+        <p><i>"Display a joke with the color of sky"</i></p>
+        <v-textarea class="mt-4" label="Insert prompt here" v-model="input" variant="outlined" clearable></v-textarea>
+        <v-btn class="mb-3" @click="getResponse">Get Response</v-btn>
+        <p>{{ response }}</p>
+    </v-container>
 </template>
 
 <script setup>
